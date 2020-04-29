bf7cd55782891044965fef5120ea40a230da4d76
unknown <felipeAgomes>
2020-04-29 15:26:46 -0300

    Iniciando crud com ideia de implementar uma calculadora

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import ExpenseList from './components/ExpenseList';
+import ExpenseForm from './components/ExpenseForm';
+import Alert from './components/Alert';
+import uuid from 'uuid/v4';
+
+const initialExpenses = [
+  {id: uuid(), charge: "renda", amount: 1600 },
+  {id: uuid(), charge: "forma de pagamento", amount: 400 },
+  {id: uuid(), charge: "fatura do cartão de crédito", amount: 1200 }
+];
+
+
+
 
 function App() {
+  const result = useState(initialExpenses);
+  const expenses = result[0];
+  const setExpenses = result[1];
+  console.log(expenses, setExpenses);
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Alert />
+      <ExpenseForm />
+      <ExpenseList />
     </div>
   );
 }
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ExpenseItem = () => {
+    return (
+        <div>
+            hello world ExepenseItem 
+        </div>
+    )
+}
+
+export default ExpenseItem;
